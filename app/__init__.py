dfc9b2c9331f0e4d31e8c36449f2109493499694
gus <ggaidelevicius@gmail.com>
2021-11-27 16:34:54 +0800

    its good to go

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['RECAPTCHA_USE_SSL']= False
+app.config['RECAPTCHA_PUBLIC_KEY']= '6LcQIGEdAAAAAE6GelsofZ-4kbRP_alrV3rlrIgQ'
+app.config['RECAPTCHA_PRIVATE_KEY']='6LcQIGEdAAAAAAJ03C5W6zcjuV4VRxzHIOrIw61G'
+app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, FileField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 class RosterForm(FlaskForm):
-    username = StringField('Username:', validators=[DataRequired()])
-    rostertype = RadioField('Roster Type:',choices=['ED','MAPU'] ,validators=[DataRequired()])
-    roster = FileField('Upload Roster', validators=[DataRequired()])
-    submit = SubmitField('Run Rostro')
+    username = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "enter your name as it appears on your roster"})
+    rostertype = RadioField('Roster type',choices=['ED','MAPU'] ,validators=[DataRequired()])
+    roster = FileField('Upload roster', validators=[DataRequired()])
+    recaptcha = RecaptchaField()
+    submit = SubmitField('SUBMIT')
