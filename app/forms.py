cdaa2b2f051ac30f5fc5219ecb1d9b1948353a3a
gus <ggaidelevicius@gmail.com>
2021-11-27 19:07:51 +0800

    im first

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, FileField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 class RosterForm(FlaskForm):
-    username = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "enter your name as it appears on your roster"})
+    username = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Enter your name here"})
     rostertype = RadioField('Roster type',choices=['ED','MAPU'] ,validators=[DataRequired()])
     roster = FileField('Upload roster', validators=[DataRequired()])
     # recaptcha = RecaptchaField()
