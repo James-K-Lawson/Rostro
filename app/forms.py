ede417fdc18403082330d296b05ce8c2529eadb9
gus <ggaidelevicius@gmail.com>
2021-11-27 19:43:05 +0800

    form style

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 # from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, FileField, RadioField, SubmitField
+from wtforms import StringField, FileField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class RosterForm(FlaskForm):
     username = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Enter your name here"})
-    rostertype = RadioField('Roster type',choices=['ED','MAPU'] ,validators=[DataRequired()])
+    rostertype = SelectField('Roster type',choices=['ED','MAPU'] ,validators=[DataRequired()])
     roster = FileField('Upload roster', validators=[DataRequired()])
     # recaptcha = RecaptchaField()
     submit = SubmitField('SUBMIT')
