3034c1a759612da5cf803b870f983a76aeafea28
James-K-Lawson <jkl2197@gmail.com>
2021-12-14 20:31:19 +1000

    hello v7

--- RostroBackend/Rostro.py
+++ RostroBackend/Rostro.py
@@ -18,11 +18,8 @@ class ArgumentParser(argparse.ArgumentParser):
         self.print_help(sys.stderr)
         self.exit(2, "%s: error: %s\n" % (self.prog, message))
 
-def sort_roster(username: str, rostertype: str, rosterpath: str):
-    if rostertype == 'ED':
-        ros = Roster(username, rosterpath)
-    if rostertype == 'MAPU':
-        ros = Roster(username, rosterpath)
+def sort_roster(username: str, rosterpath: str):
+    ros = Roster(username, rosterpath)
     return ros
 
 if __name__ == "__main__":
@@ -30,7 +27,6 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="args for Rostro")
     parser.add_argument("username", type=str, help="hint for username")
     parser.add_argument("rosterpath", type=str, help="hint for rostername")
-    parser.add_argument("rostertype", type=str, help="Enter either 'ED' or 'MAPU'")
     args = parser.parse_args()
-    ed = sort_roster(args.username, args.rostertype, args.rosterpath)
+    ed = sort_roster(args.username, args.rosterpath)
     ed.create_ical()
